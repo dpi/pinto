293628abf83b790bc1f11d7eba0bde175bddc971
dpi <pro@danielph.in>
2025-04-02 15:25:25 +0800

    Rename method so `final create` doesnt use up valuable namespace

--- src/CanonicalProduct/CanonicalFactoryTrait.php
+++ src/CanonicalProduct/CanonicalFactoryTrait.php
@@ -8,7 +8,7 @@ use Pinto\PintoMapping;
 
 trait CanonicalFactoryTrait
 {
-    final public static function create(mixed ...$args): self
+    final public static function factoryCreate(mixed ...$args): self
     {
         $className = self::pintoMappingStatic()->getCanonicalObjectClassName(self::class);
         if (null === $className) {
